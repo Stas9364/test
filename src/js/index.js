6a8dfda2178089b1e0d940db27557e2489648d32
Stas <foxxx.9364@gmail.com>
2024-06-17 16:47:50 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import "../scss/index.scss";
+
+import Glide from "@glidejs/glide";
+
+  new Glide(".glide", {
+    type: "slider",
+    // autoplay: 2000,
+    animationDuration: 0,
+    // animationDuration: 600,
+    hoverpause: false,
+    gap: 0,
+  }).mount();
+
