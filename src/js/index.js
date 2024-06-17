348bd40f1b59d45c78231f03507352e54ce72006
Stas <foxxx.9364@gmail.com>
2024-06-17 17:18:59 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,8 @@ import Glide from "@glidejs/glide";
 
   new Glide(".glide", {
     type: "slider",
-    // autoplay: 2000,
     animationDuration: 0,
     // animationDuration: 600,
-    hoverpause: false,
     gap: 0,
   }).mount();
 
